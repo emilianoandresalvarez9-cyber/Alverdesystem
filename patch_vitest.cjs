fa2573df9ba27d07745cdc092349d434cd546286
Emiliano Alvarez <emilianoandresalvarez9@gmail.com>
2026-09-24 00:26:11 -0300

    chore: cleanup patch scripts

--- patch_vitest.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('vite.config.ts', 'utf8');
-
-code = code.replace(
-  'export default defineConfig({',
-  'export default defineConfig({\n  test: {\n    exclude: [\'**/node_modules/**\', \'**/dist/**\', \'**/cypress/**\', \'**/.{idea,git,cache,output,temp}/**\', \'**/e2e/**\']\n  },'
-);
-
-fs.writeFileSync('vite.config.ts', code);
